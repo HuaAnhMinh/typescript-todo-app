53b057d1da4255493fa431578d77378dbde7f762
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-01 17:47:40 +0700

    Add delete and find todo by id

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -7,7 +7,7 @@ const todoRoute: Router = Router();
 todoRoute.post('/todos',  async (req, res) => {
   try {
     if (!isTodo(req.body)) {
-      return res.status(401).send({
+      return res.status(400).send({
         error: 'Format of todo is invalid.',
       });
     }
@@ -20,26 +20,64 @@ todoRoute.post('/todos',  async (req, res) => {
     });
   }
   catch (err) {
-    res.status(401).send({
-      error: err.toString(),
+    res.status(400).send({
+      error: err.message,
     });
   }
 });
 
 todoRoute.get('/todos', (req, res) => {
+  try {
 
+  }
+  catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
 });
 
 todoRoute.get('/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      res.status(404).send({
+        error: `Found no todo with id ${req.params.id}`,
+      });
+    }
 
+    res.send({
+      todo,
+    });
+  }
+  catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
 });
 
 todoRoute.patch('/todos/:id', async (req, res) => {
+  try {
 
+  }
+  catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
 });
 
 todoRoute.delete('/todos/:id', async (req, res) => {
-
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  }
+  catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
 });
 
 export default todoRoute;
